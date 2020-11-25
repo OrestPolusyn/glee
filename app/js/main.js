bec23da9f07313ea0b7c7a601e7b7c477ba2876c
OrestPolusyn <opect.pol@i.ua>
2020-11-25 16:31:31 +0200

    finish page

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,30 @@
-$(function () {
+$(function() {
 
-  $('.slider').slick({
-    dots: true,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000
-  });
-  $(function () {
-    var mixerDesign = mixitup('.design__products');
-    var mixerDesign = mixitup(containerEl, {
-      selectors: {
-        target: '.blog-item'
-      },
-      animation: {
-        duration: 300
-      }
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
     });
-  });
-  var mixerProducts = mixitup('.product__items');
-  var mixerProducts = mixitup(containerEl, {
-    selectors: {
-      target: '.product__item'
-    },
-    animation: {
-      duration: 300
-    }
-  });
+
+    var mixerDesign = mixitup('.design__products', {
+        selectors: {
+            control: '.design .list__nav-item__link'
+        },
+        animation: {
+            duration: 300
+        }
+    });
+
+    var mixerProducts = mixitup('.product__items', {
+        selectors: {
+            control: '.product .list__nav-item__link'
+        },
+        animation: {
+            duration: 300
+        }
+    });
 
 
 });
