93b370d83d9b56b10d723007def73f55afd2cae0
Орест Полусин <kingorest@gmail.com>
2020-12-03 08:45:07 +0200

    add pagination

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,32 @@
-$(function() {
+$(function () {
+
+    $(".star").rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+    });
+    
+    $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 800,
+        from: 100,
+        to: 500,
+        step: 0.01,
+        prettify_enabled: true,
+        prettify_separator: '.',
+
+        onChange: function (data) {
+            $('.filter-price__from').text(data.from_pretty);
+            $('.filter-price__to').text(data.to_pretty);
+        },
+        onStart: function (data) {
+            $('.filter-price__from').text(data.from_pretty);
+            $('.filter-price__to').text(data.to_pretty);
+        }
+    });
+
 
     $('.slider-top').slick({
         dots: true,
