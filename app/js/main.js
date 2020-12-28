5d1e044b970af8e6fa82c1d7136bdb738ca7adf9
Орест Полусин <kingorest@gmail.com>
2020-12-28 09:28:13 +0200

    add map

--- app/js/main.js
+++ app/js/main.js
@@ -91,9 +91,138 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {
-      lat: 29.750815,
-      lng: -95.308150
+      lat: 29.773931466944383,
+      lng: -95.37371271398355
     },
-    zoom: 13,
+    zoom: 9,
+    styles: [{
+        "elementType": "geometry",
+        "stylers": [{
+            "hue": "#ff4400"
+          },
+          {
+            "saturation": -68
+          },
+          {
+            "lightness": -4
+          },
+          {
+            "gamma": 0.72
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon"
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [{
+            "hue": "#0077ff"
+          },
+          {
+            "gamma": 3.1
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "stylers": [{
+            "hue": "#00ccff"
+          },
+          {
+            "gamma": 0.44
+          },
+          {
+            "saturation": -33
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "stylers": [{
+            "hue": "#44ff00"
+          },
+          {
+            "saturation": -23
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+            "hue": "#007fff"
+          },
+          {
+            "gamma": 0.77
+          },
+          {
+            "saturation": 65
+          },
+          {
+            "lightness": 99
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+            "gamma": 0.11
+          },
+          {
+            "weight": 5.6
+          },
+          {
+            "saturation": 99
+          },
+          {
+            "hue": "#0091ff"
+          },
+          {
+            "lightness": -86
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [{
+            "lightness": -48
+          },
+          {
+            "hue": "#ff5e00"
+          },
+          {
+            "gamma": 1.2
+          },
+          {
+            "saturation": -23
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+            "saturation": -64
+          },
+          {
+            "hue": "#ff9100"
+          },
+          {
+            "lightness": 16
+          },
+          {
+            "gamma": 0.47
+          },
+          {
+            "weight": 2.7
+          }
+        ]
+      }
+    ]
   });
 }
